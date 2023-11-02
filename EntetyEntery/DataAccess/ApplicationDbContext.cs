879669f577f73d17d614de2a0acaf347cccb3040
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-02 14:49:39 +0500

    Add project files.

---
+++ EntetyEntery/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EntetyEntery.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntetyEntery.DataAccess
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Students> Students {  get; set; } 
+    }
+}
---
+++ EntetyEntery/Models/Students.cs
@@ -0,0 +1,11 @@
+﻿namespace EntetyEntery.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname {  get; set; }
+        public int Year { get; set; }
+
+    }
+}
